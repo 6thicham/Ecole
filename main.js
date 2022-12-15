c2ba7b3f86e62f54777760c698b3807562e10bf5
6thicham <114611161+6thicham@users.noreply.github.com>
2022-12-15 22:56:02 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let myHeading = document.querySelector('h1');
+myHeading.textContent = 'Bonjour, monde !';
+
+let x = 2 
+let y = 2
+if (x===y){
+    console.log("type good");
+}
+else if(x==y){
+    console.log("egaux en valeur");
+}
+else {
+    console.log("")
+}
